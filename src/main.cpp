ebbdf2b4f9b284ff4728f92c15f734d16e50b2e9
OhGaGaGaGa <171927821@qq.com>
2022-10-13 20:09:06 +0800

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,38 @@
 #include <igl/readOBJ.h>
 #include <igl/writeOBJ.h>
 #include <igl/solid_angle.h>
+#include "Constants.h"
 #include "Meshs.h"
+#include "VTKwriter.h"
 
 using namespace std;
 
-double randomFromTo(int left, int right) {
+double random_from_to(double left, double right) {
     return left + (right - left) * rand() / RAND_MAX;
 }
 
-Eigen::MatrixXd randomPoints(int cnt, double l0, double r0, double l1, double r1, double l2, double r2) {
+Eigen::MatrixXd random_points(int cnt, double l0, double r0, double l1, double r1, double l2, double r2) {
     Eigen::MatrixXd ret(cnt, 3);
     for (int i = 0; i < cnt; i++) {
-        ret(i, 0) = randomFromTo(l0, r0);
-        ret(i, 1) = randomFromTo(l1, r1);
-        ret(i, 2) = randomFromTo(l2, r2);
+        ret(i, 0) = random_from_to(l0, r0);
+        ret(i, 1) = random_from_to(l1, r1);
+        ret(i, 2) = random_from_to(l2, r2);
     }
     return ret;
 }
 
 int main() {
-    srand(time(0));
+    srand(10);
 
     Eigen::MatrixXd inputV;
     Eigen::MatrixXi inputF;
-    igl::readOBJ("./files/tiger-in.obj", inputV, inputF);
+    igl::readOBJ("../files/tiger-in.obj", inputV, inputF);
+
+    std::cout << inputF.cols() << std::endl;
+
+    for (int i = 0; i < inputF.cols(); i++)
+        std::cout << inputF(0, i) << " ";
+    std::cout << std::endl;
 
     Meshs meshs(inputV, inputF);
 
@@ -43,23 +51,28 @@ int main() {
         max2 = std::max(max2, inputV(i, 2));
     }
 
-    const double Padding = 10;
-    const int TestSize = 100;
+    cout << min0 << " " << max0 << endl;
+    cout << min1 << " " << max1 << endl;
+    cout << min2 << " " << max2 << endl;
 
-    Eigen::MatrixXd testV = randomPoints(TestSize,
+    Eigen::MatrixXd testV = random_points(TestSize,
         min0 - Padding, max0 + Padding,
         min1 - Padding, max1 + Padding,
         min2 - Padding, min2 + Padding
     );
 
-    double judge[TestSize];
+    std::array<double, TestSize> judge{ 0 };
 
     for (int i = 0; i < testV.rows(); i++) {
         Eigen::VectorXd tmpVec(3);
         tmpVec << testV(i, 0), testV(i, 1), testV(i, 2);
         judge[i] = meshs.calc_winding_value(tmpVec);
+
+        std::cout << judge[i] << " ";
     }
 
+    VTKwriter writer("../files/tiger-out.vtk", testV, judge);
+    writer.write_colored_points();
 
     return 0;
 }
