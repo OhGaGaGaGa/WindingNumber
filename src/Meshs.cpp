c6be8433c699371b4d456a5c1e2e98c6cbbc72c0
OhGaGaGaGa <171927821@qq.com>
2022-11-01 14:10:44 +0800

    tmp save

--- src/Meshs.cpp
+++ src/Meshs.cpp
@@ -16,9 +16,27 @@ double Meshs::calc_winding_value(const Eigen::Vector3d& p) {
 void Meshs::init_aabb_tree() {
     igl::AABB<Eigen::MatrixXd, 3> tree;
     tree.init(_vertex, _mesh);
-    Eigen::VectorXd sqrD;
-    Eigen::VectorXi I;
-    Eigen::MatrixXd C;
-    Eigen::Vector3d P;
+
+    Eigen::MatrixXd P; // p * 3
+    Eigen::VectorXd sqrD; // p * 1, smallest squared distances
+    Eigen::VectorXi I; // p * 1, primitive indices corresponding to smallest distances
+    Eigen::MatrixXd C; // p * 3, closest points
     tree.squared_distance(_vertex, _mesh, P, sqrD, I, C);
+}
+
+void Meshs::init_octree() {
+    igl::per_face_normals(_vertex, _mesh, _face_normal);
+
+    std::array<double, 3> min_axis {_vertex.col(0).minCoeff(), _vertex.col(1).minCoeff(), _vertex.col(2).minCoeff()};
+    std::array<double, 3> max_axis {_vertex.col(0).maxCoeff(), _vertex.col(1).maxCoeff(), _vertex.col(2).maxCoeff()};
+
+    OcTree tree(min_axis, max_axis);
+
+    for (auto i = 0; i < _mesh.rows(); i++) {
+        tree.insert(_mesh.row(i));
+    }
+}
+
+bool OcTree::insert(const Eigen::Vector3i& triangle) {
+    
 }
