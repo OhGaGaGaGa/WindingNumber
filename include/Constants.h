64156a06a6d6f7db10b8a0ca59fd9ba1f6420be0
OhGaGaGaGa <171927821@qq.com>
2022-11-02 18:35:25 +0800

    More cubes in grid

--- include/Constants.h
+++ include/Constants.h
@@ -5,7 +5,7 @@ const double PADDING = 0.1;
 const int TESTSIZE = 200;
 const double EPS = 1e-5;
 const double ISO_VALUE = -0.2;
-const long DIVI_X = 20, DIVI_Y = 20, DIVI_Z = 20;
+const long DIVI_X = 100, DIVI_Y = 100, DIVI_Z = 100;
 const int OCTREE_MAX_DEPTH = 5;
 const double ACCURACY = 3;
 
